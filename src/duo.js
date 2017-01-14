49e67dc52ea5af88ef39a410081166fc08b79bce
Kevin Lin <developer@kevinlin.info>
2017-01-13 19:01:54 -0600

    API change: passed callback is invoked with sig response

--- src/duo.js
+++ src/duo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import omit from 'object.omit';
 
 import duoWeb from '../lib/duo-web';
 
@@ -9,29 +10,51 @@ import duoWeb from '../lib/duo-web';
  */
 export default class Duo extends React.Component {
   static propTypes = {
+    // The Duo API host
     host: React.PropTypes.string,
+    // The server-side generated sig request to pass as input to Duo to begin the 2FA flow
     sigRequest: React.PropTypes.string,
-    submitCallback: React.PropTypes.func
+    // A callback function invoked with a sig response from Duo when the 2FA flow is complete
+    sigResponseCallback: React.PropTypes.func
   };
   static defaultProps = {
-    submitCallback: () => {}
+    sigResponseCallback: () => {}
   };
 
   componentDidMount() {
-    const {host, sigRequest, submitCallback} = this.props;
+    const {host, sigRequest} = this.props;
 
     duoWeb.duoWeb().init({
       /* eslint-disable camelcase */
       host,
       sig_request: sigRequest,
-      submit_callback: submitCallback
+      submit_callback: this.handleDuoResponse.bind(this)
       /* eslint-enable camelcase */
     });
   }
 
+  /**
+   * On 2FA flow completion, the Duo Web SDK invokes the passed submission callback with a form
+   * DOM element containing the elements that would otherwise be submitted to another server-side
+   * endpoint. This doesn't make much sense for a client-side rendered SPA, and the only field
+   * we're really concerned about in the form is Duo's sig response. Thus, the callback passed to
+   * this component will be invoked with the sig response as a string.
+   *
+   * @param {Object} duoForm Duo form DOM element.
+   * @returns {*} Return value is unused.
+   */
+  handleDuoResponse(duoForm) {
+    const {sigResponseCallback} = this.props;
+    const sigResponse = duoForm.firstChild.value;
+
+    return sigResponseCallback(sigResponse);
+  }
+
   render() {
+    const props = omit(this.props, Object.keys(Duo.propTypes));
+
     return (
-      <iframe id="duo_iframe" />
+      <iframe id="duo_iframe" {...props} />
     );
   }
 }
--- test/test-duo.js
+++ test/test-duo.js
@@ -16,8 +16,8 @@ test('Duo SDK is initialized and component is rendered correctly', (t) => {
   });
   const duo = mount(
     <Duo
-      host={'host'}
-      sigRequest={'sig request'}
+      host="host"
+      sigRequest="sig request"
     />
   );
 
@@ -27,5 +27,53 @@ test('Duo SDK is initialized and component is rendered correctly', (t) => {
   t.equal(iframe.length, 1, 'Duo iframe is present');
   t.equal(iframe.props().id, 'duo_iframe', 'Default node ID is correct');
 
+  duoWeb.duoWeb.restore();
+  t.end();
+});
+
+test('Callback is invoked with Duo\'s sig response', (t) => {
+  const libStub = sinon.stub(duoWeb, 'duoWeb').returns({
+    init: (opts) => opts.submit_callback({
+      // Simulation of properties in a DOM form element
+      firstChild: {
+        value: 'sig response'
+      }
+    })
+  });
+  const cb = sinon.spy();
+  const duo = mount(
+    <Duo
+      sigResponseCallback={cb}
+    />
+  );
+
+  t.ok(libStub.called, 'Library is initialized');
+  t.ok(cb.calledWith('sig response'), 'Callback is invoked with the sig response');
+  t.equal(duo.find('iframe').length, 1, 'Callback does not affect rendered elements');
+
+  duoWeb.duoWeb.restore();
+  t.end();
+});
+
+test('Extraneous props are proxied through to the iframe', (t) => {
+  const libStub = sinon.stub(duoWeb, 'duoWeb').returns({
+    init: () => {}
+  });
+  const duo = mount(
+    <Duo
+      host="host"
+      sigRequest="sig request"
+      className="class name"
+      style={{color: 'red'}}
+    />
+  );
+
+  const iframe = duo.find('iframe');
+  t.equal(iframe.length, 1, 'Duo iframe is still present');
+  t.ok(libStub.called, 'Library is initialized');
+  t.equal(iframe.props().className, 'class name', 'Class name is proxied');
+  t.deepEqual(iframe.props().style, {color: 'red'}, 'Style is proxied');
+
+  duoWeb.duoWeb.restore();
   t.end();
 });
